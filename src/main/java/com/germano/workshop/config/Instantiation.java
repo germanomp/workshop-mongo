e6b9b06588afe8ad9d0360fea3ca0e8d71318782
Germano Manzoni <germano12344@gmail.com>
2024-11-07 12:06:21 -0300

    Comentários nos posts

--- src/main/java/com/germano/workshop/config/Instantiation.java
+++ src/main/java/com/germano/workshop/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.germano.workshop.config;
 import com.germano.workshop.domain.Post;
 import com.germano.workshop.domain.User;
 import com.germano.workshop.dto.AuthorDTO;
+import com.germano.workshop.dto.CommentDTO;
 import com.germano.workshop.repository.PostRepository;
 import com.germano.workshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/08/2023"), "Partiu viagem", "Vou viajar para são paulo.", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/08/2023"), "Bom dia", "Acordei agora.", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem", sdf.parse("21/08/2023"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("23/08/2023"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um bom dia", sdf.parse("21/08/2023"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
