cef1a08816180a0b546199ac93fc9aa780d7b4bc
Germano Manzoni <germano12344@gmail.com>
2024-11-07 12:25:31 -0300

    consulta simples com query methods

--- src/main/java/com/germano/workshop/resources/PostResource.java
+++ src/main/java/com/germano/workshop/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.germano.workshop.resources;
 import com.germano.workshop.domain.Post;
 import com.germano.workshop.domain.User;
 import com.germano.workshop.dto.UserDTO;
+import com.germano.workshop.resources.util.URL;
 import com.germano.workshop.services.PostService;
 import com.germano.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
