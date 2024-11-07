149317128ba8d7b09216084cec83497c902dec06
Germano Manzoni <germano12344@gmail.com>
2024-11-07 11:45:11 -0300

    obtendo post por id

---
+++ src/main/java/com/germano/workshop/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.germano.workshop.resources;
+
+import com.germano.workshop.domain.Post;
+import com.germano.workshop.domain.User;
+import com.germano.workshop.dto.UserDTO;
+import com.germano.workshop.services.PostService;
+import com.germano.workshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/germano/workshop/services/PostService.java
@@ -0,0 +1,26 @@
+package com.germano.workshop.services;
+
+import com.germano.workshop.domain.Post;
+import com.germano.workshop.domain.User;
+import com.germano.workshop.dto.UserDTO;
+import com.germano.workshop.repository.PostRepository;
+import com.germano.workshop.repository.UserRepository;
+import com.germano.workshop.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
